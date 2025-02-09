8dce02fb5ab3f1510bcc7c744f5561efdd5260fb
Woraprat <75469218+bestchanel@users.noreply.github.com>
2025-02-09 20:10:38 +0700

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,16 @@ export default ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      host: env('MYSQL_ADDON_HOST'),
+      port: env.int('MYSQL_ADDON_PORT'),
+      database: env('MYSQL_ADDON_DB'),
+      user: env('MYSQL_ADDON_USER'),
+      password: env('MYSQL_ADDON_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false),
+    },
+    pool: {
+      min: 0, // จำนวนการเชื่อมต่อขั้นต่ำ
+      max: 5, // จำกัดจำนวนการเชื่อมต่อสูงสุด
     },
   },
 });
